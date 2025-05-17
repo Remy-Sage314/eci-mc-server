bd781d720138c94a4e492e205822933897d24c9a
wddcxy <wddcxy@126.com>
2025-05-17 15:17:12 +0800

    change todo

--- B/discord/__init__.py
+++
@@ -1,7 +0,0 @@
-from nonebot.plugin import PluginMetadata
-
-__plugin_meta__ = PluginMetadata(
-    name="EMCS_discord",
-    description="",
-    usage=""
-)
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from alicebot import Bot
-from alicebot.adapter.dingtalk import DingTalkAdapter
-
-bot = Bot()
-bot.load_adapters(DingTalkAdapter)
-
-if __name__ == "__main__":
-    bot.run()
